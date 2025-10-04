a33cc7ae8c59824afa509bfc561a9c339bdd9438
ShyamTandel <shyamtandel323@gmail.com>
2025-10-04 16:42:01 +0530

    Alter OptionPosition model table name and update database settings to use MySQL

--- api/models.py
+++ api/models.py
@@ -15,5 +15,8 @@ class OptionPosition(models.Model):
     remote_order_id = models.BigIntegerField(null=True, blank=True)
     active = models.BooleanField(default=True)
 
+    class Meta:
+        db_table = 'option_position'
+        
     def __str__(self):
         return f"{self.symbol} {self.side} delta={self.delta}"
