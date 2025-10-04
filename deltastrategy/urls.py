432dcd7ba8682ae8cb4a9fd836aac2ec15a13120
shyam-tandel <shyam.t@peerbits.com>
2025-10-04 11:47:23 +0530

    Remove unused API views, models, and serializers; update settings to load environment variables and allow all CORS origins.

--- deltastrategy/urls.py
+++ deltastrategy/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls')),  # For browsable API authentication
 ]
