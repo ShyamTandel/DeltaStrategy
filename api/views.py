07b9d1bbd06997bfd6a3e3a50eb37ea581f44ab3
shyam-tandel <shyam.t@peerbits.com>
2025-10-04 11:51:50 +0530

    Add home view and templates for the landing page; update URL routing and settings for template directory

--- api/views.py
+++ api/views.py
@@ -5,9 +5,19 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 import json
 
 # Create your views here.
+
+def home_view(request):
+    """
+    Home page view
+    """
+    context = {
+        'current_time': timezone.now(),
+    }
+    return render(request, 'home.html', context)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
